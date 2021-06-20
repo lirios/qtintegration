34cb9d7092e0a8b9983b50334454351c40946f43
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2021-06-20 15:28:23 +0200

    Add SPDX license information

--- src/resourcehelper.h
+++ src/resourcehelper.h
@@ -1,28 +1,7 @@
-/****************************************************************************
- * This file is part of Liri.
- *
- * Copyright (C) 2015-2016 Pier Luigi Fiorini
- * Copyright (C) 2015 The Qt Company Ltd.
- *
- * Author(s):
- *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
- *
- * $BEGIN_LICENSE:GPL3$
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * $END_LICENSE$
- ***************************************************************************/
+// Copyright (C) 2015 The Qt Company Ltd.
+// Copyright (C) 2015-2016 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #pragma once
 
